a48e7a4b6dea81f4f219596a64dead713a54549e
Mick Pollard <aussielunix@gmail.com>
2015-03-24 15:46:53 +1100

    initial checkin

---
+++ README.md
@@ -0,0 +1,4 @@
+# BitchX Ubuntu Package
+
+This uses fpm-cookery and circleci to build DEB packages of [BitchX](https://github.com/BitchX/BitchX1.3)
+
---
+++ recipe.rb
@@ -0,0 +1,31 @@
+class BitchX < FPM::Cookery::Recipe
+
+  homepage      'http://www.bitchx.com/'
+
+  name          'BitchX'
+  arch          'amd64'
+  version       '1.3.0'
+  revision      '1'
+  vendor        'aussielunix'
+
+  description   "BitchX is a free software text-based IRC (Internet Relay Chat) client for UNIX-like systems, originally based on ircII and heavily influenced by EPIC."
+  section       ''
+
+  source        'https://github.com/BitchX/BitchX1.3', :with => :git, :sha => '6c7e32a97d3ac411f43741f08fca350b529ee7e4'
+
+  build_depends [ 'libssl-dev', 'libncurses5-dev' ]
+
+  def build
+    configure(
+      :with-ssl         => true,
+      :with-plugins     => true,
+      :enable-ipv6      => true,
+    )
+    make
+  end
+
+  def install
+    make :install
+  end
+end
+
